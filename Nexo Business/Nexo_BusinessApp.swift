7315aeecda16052f3a96dc425284967722ff4826
José Ruiz <ruiz211@protonmail.com>
2026-05-29 11:22:16 -0500

    Initial Commit

---
+++ Nexo Business/Nexo_BusinessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Nexo_BusinessApp.swift
+//  Nexo Business
+//
+//  Created by José Ruiz on 29/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Nexo_BusinessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
